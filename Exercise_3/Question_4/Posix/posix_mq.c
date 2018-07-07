2b40e6acd1850957b8f7a103c68d6bb96b585e7e
Monish.Nene <monish.nene@colorado.edu>
2018-07-06 20:55:28 -0600

    updated posix_mq

--- Exercise_3/Question_4/Posix/posix_mq.c
+++ Exercise_3/Question_4/Posix/posix_mq.c
@@ -1,83 +1,90 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<errno.h>
-#include<pthread.h>
-#include<mqueue.h>
-#include<sys/stat.h>
-#include<sys/stat.h>
-#include<fcntl.h>
-#include<mqueue.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <mqueue.h>
+#include <unistd.h>
 
 #define SNDRCV_MQ	"/send_receive_mq"
-#define ERROR	(-1)
-
-
-pthread_t send_thread, receive_thread;
+#define BUFFER_SIZE 100
+#define ERROR (-1)
 
 struct mq_attr mq_attr;
 
-pthread_attr_t att[2];
+pthread_t thread_receive, thread_send;
 
-struct sched_param parameter[2];
+pthread_attr_t attribute_receive, attribute_send;
 
-
-static char canned_msg[] = "This is a test, and only a test, in the event of real emergency, you would be instructed...."; // Message that is to be sent by sender for posix_mq
-
-void *sender_function(void *arg){
-mqd_t mymq;
-int prio;
-int nbytes;
-
-mymq=mq_open(SNDRCV_MQ, O_RDWR, 0664, &mq_attr);	// read write permission using O_RDWR
-
-if((nbytes = mq_send(mymq, canned_msg, sizeof(canned_msg),30))== ERROR){	// Error checking while sending
-printf("mq_send");
-}
-else{	// Message is being sent successfully
-printf("send: message successfully sent\n");
-}
-}
+struct sched_param parameter_receive, parameter_send;	
 
 
-void *receiver_function(void *arg){
-mqd_t mymq;
-char buffer[128];
-int prio;
-int nbytes;
-mymq = mq_open(SNDRCV_MQ, O_CREAT|O_RDWR, 0664, &mq_attr); //mq_open function with its parameters 's output being returned to mymq
+static char canned_msg[] = "This is a test, and only a test, in the event of real emergency, you would be instructed...."; // Message to be sent
 
-if((nbytes = mq_receive(mymq,buffer, 128 , &prio)) == ERROR)	// Error checking while receiving
+void *receiver(void *arg)
 {
-printf("mq_receive");
+	mqd_t mymq;
+	char buffer[BUFFER_SIZE];		
+	int prio;
+	int nbytes;
+	mymq = mq_open(SNDRCV_MQ, O_CREAT|O_RDWR, 0664, &mq_attr); 
+	if((nbytes = mq_receive(mymq,buffer, BUFFER_SIZE, &prio)) == ERROR)	
+	{
+		printf("mq_receive");
+	}
+	else
+	{	// Message received successfully
+		buffer[nbytes]='\0';
+		printf("recive:msg %s received with priority =%d, length =%d\n",buffer, prio, nbytes);
+	}
 }
-else
+
+
+void *sender(void *arg)
 {
-// Message is being received successfully
-buffer[nbytes]='\0';
-printf("recive:msg %s received with priority =%d, length =%d\n",buffer, prio, nbytes);
+	mqd_t mymq;
+	int prio;
+	int nbytes;
+
+	mymq=mq_open(SNDRCV_MQ, O_RDWR, 0664, &mq_attr);	// Create a message queue with name SNDRCV_MQ with read write permission
+
+	if((nbytes = mq_send(mymq, canned_msg, sizeof(canned_msg),30))== ERROR)
+	{	
+		printf("mq_send");
+	}
+	else
+	{	
+		printf("send: message successfully sent\n");
+	}
 }
-}
 
-void main(){
-int i=0;
-for(i=0;i<2;i++){
-pthread_attr_init(&att[i]);	// Attribute initialisation for both threads
-pthread_attr_setinheritsched(&att[i],PTHREAD_EXPLICIT_SCHED);//Setting the scheduling policy
-pthread_attr_setschedpolicy(&att[i], SCHED_FIFO);//Scheduling policy is set to SCHED_FIFO
-parameter[i].sched_priority=99-i;	//Priority assignment
-pthread_attr_setschedparam(&att[i],&parameter[i]);	// Assigning the remaining parameters
-}
+void main()
+{
+	int i=0,rc=0;
+	mq_attr.mq_maxmsg=10;		//Message numbers
+	mq_attr.mq_msgsize= BUFFER_SIZE;	//Message size
 
+	mq_attr.mq_flags=0;
 
-mq_attr.mq_maxmsg=10;		//number of messages
-mq_attr.mq_msgsize= 128;	//size of a message
+	rc=pthread_attr_init(&attribute_receive);
+  	rc=pthread_attr_setinheritsched(&attribute_receive, PTHREAD_EXPLICIT_SCHED);
+  	rc=pthread_attr_setschedpolicy(&attribute_receive, SCHED_FIFO);
+  	parameter_receive.sched_priority=10;
+  	pthread_attr_setschedparam(&attribute_receive, &parameter_receive);
 
-mq_attr.mq_flags=0;
+  	rc=pthread_attr_init(&attribute_send);
+  	rc=pthread_attr_setinheritsched(&attribute_send, PTHREAD_EXPLICIT_SCHED);
+  	rc=pthread_attr_setschedpolicy(&attribute_send, SCHED_FIFO);
+  	parameter_send.sched_priority=9;
+  	pthread_attr_setschedparam(&attribute_send, &parameter_send);
 
-pthread_create(&receive_thread, &att[0], receiver_function, NULL);	// Creating  the receive thread
+	if(pthread_create(&thread_receive, &attribute_receive, receiver, NULL)==0)
+		printf("\n\rReceiver thread created\n\r");
+  	else perror("\n\rReceiver thread creation failed\n\r");
 
-pthread_create(&send_thread, &att[1] ,sender_function, NULL);	//Creating the sender thread
+	if(pthread_create(&thread_send, &attribute_send, sender, NULL)==0)
+		printf("\n\rSender thread created\n\r");
+  	else perror("\n\rSender thread creation failed\n\r");
 
-pthread_join(receive_thread,NULL);
-pthread_join(send_thread,NULL);
+  	pthread_join(thread_receive, NULL);
+  	pthread_join(thread_send, NULL);
 }
